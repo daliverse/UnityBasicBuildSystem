b918dd678f0277e388ef2a3068039c7080b4b566
Darien Large <dlarge@daliverse.com>
2018-11-05 17:35:54 -0600

    initial commit

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+
+    Hashtable prefabsToBuild = new Hashtable();
+
+    public GameObject prefab1, prefab2, prefab3, prefab4;
+
+    //fixme: get runtime build height
+    public float buildHeight = 0.6f;
+    float buildY;
+
+    KeyCode autoBuildKey = KeyCode.LeftShift;
+
+    // Use this for initialization
+    void Start()
+    {
+        buildY = buildHeight;
+
+        prefabsToBuild.Add(KeyCode.Alpha1, prefab1);
+        prefabsToBuild.Add(KeyCode.Alpha2, prefab2);
+        prefabsToBuild.Add(KeyCode.Alpha3, prefab3);
+        prefabsToBuild.Add(KeyCode.Alpha4, prefab4);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Input.inputString contains a string of all the keys entered during this frame, might be better
+        if (Input.GetKeyDown(KeyCode.Alpha1) || (Input.GetKey(KeyCode.Alpha1) && Input.GetKey(autoBuildKey)) ||
+            Input.GetKeyDown(KeyCode.Alpha2) || (Input.GetKey(KeyCode.Alpha2) && Input.GetKey(autoBuildKey)) ||
+            Input.GetKeyDown(KeyCode.Alpha3) || (Input.GetKey(KeyCode.Alpha3) && Input.GetKey(autoBuildKey)) ||
+            Input.GetKeyDown(KeyCode.Alpha4) || (Input.GetKey(KeyCode.Alpha4) && Input.GetKey(autoBuildKey)))
+        {
+            foreach (KeyCode k in prefabsToBuild.Keys)
+            {
+                //Debug.Log("Checking " + k);
+                if (Input.GetKeyDown(k) || (Input.GetKey(k) && Input.GetKey(autoBuildKey)))
+                {
+                    Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+                    Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+                    Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+                    curPosition.y = buildY;
+                    Instantiate((GameObject)prefabsToBuild[k], curPosition, transform.rotation);
+
+                    //Debug.Log("Built at " + curPosition);
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    private bool pickedUp;
+
+    private Vector3 initialPos;
+
+    //the max distance from the object's spawn point before it's subject to destruction
+    public float killDist;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        initialPos = transform.position;
+        InvokeRepeating("CheckSelfDestruct", 10f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+
+    //Must have a collider (non-trigger)
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        PickedUp = true;
+    }
+
+    // Called every frame when the user has clicked on a GUIElement or Collider and is still holding down the mouse.
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+
+    void OnMouseUp()
+    {
+        PickedUp = false;
+    }
+
+
+    public bool PickedUp
+    {
+        get
+        {
+            return pickedUp;
+        }
+
+        set
+        {
+            //Debug.Log(value ? "Picked Up" : "Dropped");
+            pickedUp = value;
+        }
+    }
+
+    private void CheckSelfDestruct()
+    {
+        if (Vector3.Distance(transform.position, initialPos) > killDist)
+        {
+            //Debug.Log("Goodbye, cruel world");
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
